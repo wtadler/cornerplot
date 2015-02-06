6f954aa09ced3360661a20658d7b5033b13f538e
Will Adler <will.adler@nyu.edu>
2015-02-06 11:02:59 -0500

    cleanup

--- cornerplot.m
+++ cornerplot.m
@@ -2,25 +2,25 @@ function [fig,ax]=cornerplot(data, varargin)
 %CORNERPLOT Corner plot showing projections of a multidimensional data set.
 %
 % CORNERPLOT(DATA) plots every 2D projection of a multidimensional data
-% set. DATA is nSamples by nDimensions matrix.
+% set. DATA is an nSamples-by-nDimensions matrix.
 %
 % CORNERPLOT(DATA,NAMES) prints the names of each dimension. NAMES is a
-% cell array of strings of length nDims, or an empty cell array.
+% cell array of strings of length nDimensions, or an empty cell array.
 %
 % CORNERPLOT(DATA,NAMES,TRUTHS) indicates reference values on the plots.
-% TRUTHS is a vector of length nDims. This might be useful, for instance,
+% TRUTHS is a vector of length nDimensions. This might be useful, for instance,
 % when looking at samples from fitting to synthetic data, where true
 % parameter values are known.
 %
 % CORNERPLOT(DATA,NAMES,TRUTHS,BOUNDS) indicates lower and upper bounds for
-% each dimension. BOUNDS is a 2 by nDims matrix where the first row is the
+% each dimension. BOUNDS is a 2 by nDimensions matrix where the first row is the
 % lower bound for each dimension, and the second row is the upper bound.
 %
 % FIG is the handle for the figure, and AX is a
 % nDimensions-by-nDimensions array of all subplot handles.
 %
-% Inspired by the more excellent and full-featured triangle.py
-% (github.com/dfm/triangle.py) by Dan Foreman-Mackey (dan.iel.fm).
+% Inspired by triangle.py (github.com/dfm/triangle.py)
+% by Dan Foreman-Mackey (dan.iel.fm).
 %
 % Requires kde2d
 % (mathworks.com/matlabcentral/fileexchange/17204-kernel-density-estimation/content/kde2d.m)
@@ -30,7 +30,11 @@ function [fig,ax]=cornerplot(data, varargin)
 % Ver 1.0
 % will@wtadler.com
 
-if length(size(data))~=2
+if ~exist('kde2d','file')
+    error('You must install <a href="http://www.mathworks.com/matlabcentral/fileexchange/17204-kernel-density-estimation/content/kde2d.m">kde2d.m</a> by <a href="http://web.maths.unsw.edu.au/~zdravkobotev/">Zdravko Botev</a>.')
+end
+
+if length(size(data)) ~= 2
     error('x must be 2D.')
 end
 
@@ -48,18 +52,18 @@ bounds_supplied = true;
 
 if nargin > 1
     names = varargin{1};
-    if ~isempty(names) && ~(iscell(names) && length(names)==nDims)
+    if ~isempty(names) && ~(iscell(names) && length(names) == nDims)
         error('NAMES must be a cell array with length equal to the number of dimensions in your data.')
     end
     if nargin > 2
         truths = varargin{2};
-        if ~isempty(truths) && ~(isfloat(truths) && numel(truths)==nDims)
+        if ~isempty(truths) && ~(isfloat(truths) && numel(truths) == nDims)
             error('TRUTHS must be a vector with length equal to the number of dimensions in your data.')
         end
         if nargin > 3
             bounds = varargin{3};
-
-            if ~isempty(bounds) && ~(isfloat(bounds) && all(size(bounds)==[2 nDims]))
+            
+            if ~isempty(bounds) && ~(isfloat(bounds) && all(size(bounds) == [2 nDims]))
                 error('BOUNDS must be a 2-by-nDims matrix.')
             end
         end
@@ -72,8 +76,8 @@ if isempty(bounds)
 end
 
 % plotting parameters
-fig=figure;
-ax=nan(nDims);
+fig = figure;
+ax = nan(nDims);
 hist_bins = 100;
 lines = 10;
 res = 2^6; % defines grid for which kde2d will compute density. must be a power of 2.
@@ -83,19 +87,18 @@ axes_defaults = struct('tickdirmode','manual',...
     'ticklength',[.035 .035],...
     'box','off');
 
-
 % plot histograms
 for i = 1:nDims
     if ~bounds_supplied
         bounds(:,i) = [min(data(:,i)) max(data(:,i))];
     end
     
-    ax(i,i)=subplot_tight(1+nDims,1+nDims, sub2ind([1+nDims 1+nDims],1+i,i));
-    [n,x]=hist(data(:,i), hist_bins);
+    ax(i,i) = subplot_tight(1+nDims,1+nDims, sub2ind([1+nDims 1+nDims],1+i,i));
+    [n,x] = hist(data(:,i), hist_bins);
     plot(x,n/sum(n),'k-')
     set(gca,'yticklabel',[],'xlim',bounds(:,i),'ylim',[0 max(n/sum(n))],axes_defaults);
     
-    if i~=nDims
+    if i ~= nDims
         set(gca,'xticklabel',[])
     end
     
@@ -106,7 +109,7 @@ for i = 1:nDims
     
     if ~isempty(names)
         ylabel(sprintf('p(%s)',names{i}))
-        if i==nDims
+        if i == nDims
             xlabel(names{i})
         end
     end
@@ -115,47 +118,46 @@ end
 
 % plot projections
 if nDims > 1
-    for p1 = 1:nDims-1
-        for p2 = p1+1:nDims
-            [~, density, X, Y] = kde2d([data(:,p1) data(:,p2)],res,[bounds(1,p1) bounds(1,p2)],[bounds(2,p1) bounds(2,p2)]);
-            ax(p2,p1)=subplot_tight(1+nDims,1+nDims, sub2ind([1+nDims 1+nDims],1+p1,p2));
+    for d1 = 1:nDims-1
+        for d2 = d1+1:nDims
+            [~, density, X, Y] = kde2d([data(:,d1) data(:,d2)],res,[bounds(1,d1) bounds(1,d2)],[bounds(2,d1) bounds(2,d2)]);
+            ax(d2,d1) = subplot_tight(1+nDims,1+nDims, sub2ind([1+nDims 1+nDims],1+d1,d2));
             contour(X,Y,density, lines)
             
-
-            set(gca,'yticklabel',[],'xticklabel',[],'xlim',bounds(:,p1),'ylim',bounds(:,p2), axes_defaults);
-
+            set(gca,'yticklabel',[],'xticklabel',[],'xlim',bounds(:,d1),'ylim',bounds(:,d2), axes_defaults);
+            
             if ~isempty(truths)
                 yl = get(gca,'ylim');
                 xl = get(gca,'xlim');
                 hold on
-                plot(xl, [truths(p2) truths(p2)],'k-', 'linewidth',linewidth)
-                plot([truths(p1) truths(p1)], yl,'k-', 'linewidth',linewidth)
+                plot(xl, [truths(d2) truths(d2)],'k-', 'linewidth',linewidth)
+                plot([truths(d1) truths(d1)], yl,'k-', 'linewidth',linewidth)
             end
-            if p1==1
+            if d1 == 1
                 if ~isempty(names)
-                    ylabel(names{p2})
+                    ylabel(names{d2})
                 end
                 set(gca,'yticklabelmode','auto')
             end
-            if p2==nDims
+            if d2 == nDims
                 if ~isempty(names)
-                    xlabel(names{p1})
+                    xlabel(names{d1})
                 end
                 set(gca,'xticklabelmode','auto')
             end
         end
         
         % link axes
-        row = ax(1+p1,:);
+        row = ax(1+d1,:);
         row = row(~isnan(row));
         row = row(1:end-1);
         
-        col = ax(:,p1);
+        col = ax(:,d1);
         col = col(~isnan(col));
         
         linkaxes(row, 'y');
         linkaxes(col, 'x');
-
+        
     end
 end
 end
